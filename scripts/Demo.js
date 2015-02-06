b6dfb85296b7c87eb703fee2165f5ae3c4700c2e
Eduardo <lol.ed94@hotmail.com>
2015-02-05 18:08:10 -0600

    Started Fresh

--- scripts/Demo.js
+++
@@ -1,4 +0,0 @@
-﻿function demo(){
-    return ko.observableArray;
-}
-
--- scripts/_references.js
+++
@@ -1,3 +0,0 @@
-﻿/// <reference path="bootstrap.js" />
-/// <reference path="jquery-2.1.1.js" />
-/// <reference path="knockout-3.2.0.js" />
