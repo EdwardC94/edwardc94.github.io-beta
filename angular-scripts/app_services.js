b2b7fdbdb70dcbc87ab19faf2f551761923e5902
Eduardo <lol.ed94@hotmail.com>
2016-08-20 04:25:40 -0500

    Fixed http://edwardc94.github.io/database/it_is_mine_and_you_cannot_have_it.json 404 (Not Found)
    By adding the right path database/blog/entries/:title.json

--- angular-scripts/app_services.js
+++ angular-scripts/app_services.js
@@ -3,7 +3,7 @@
   services.
   factory('getEntry', ['$resource',
     function ($resource) {
-      return $resource('database/:title.json', {}, {
+      return $resource('database/blog/entries/:title.json', {}, {
         getEntry: {method: 'GET', params: { title : '' }, isArray: true }
       });
   }])
