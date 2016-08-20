08d43189131978cac22a85fefb7be29280093367
Eduardo <lol.ed94@hotmail.com>
2016-08-20 04:50:00 -0500

    added $routeParams as a dependency on BlogEntryCtrl

--- angular-scripts/app_controllers.js
+++ angular-scripts/app_controllers.js
@@ -2,7 +2,7 @@
     angular.module('MyWebsiteCtrls', ['ngRoute']).
     controller('HomeCtrl', ['$scope', function ($scope) {
     }]).
-    controller('BlogEntryCtrl', ['$scope', 'data', '$route', function($scope, $route, data) {
+    controller('BlogEntryCtrl', ['$scope', 'data', '$route', '$routeParams', function($scope, $route, $routeParams, data) {
         $scope.entry = data;
         $scope.contentUrl = 'database/blog/entries/' + $route.current.params.title + '.html';
     }])
