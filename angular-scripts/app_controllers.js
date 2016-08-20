11c423a83f4ea0c3431bc97e8270c6d29f332c23
Eduardo <lol.ed94@hotmail.com>
2016-08-20 04:52:54 -0500

    test

--- angular-scripts/app_controllers.js
+++ angular-scripts/app_controllers.js
@@ -4,6 +4,7 @@
     }]).
     controller('BlogEntryCtrl', ['$scope', 'data', '$route', '$routeParams', function($scope, $route, $routeParams, data) {
         $scope.entry = data;
+        console.log('$route : ' + $route + '\n $routeParams : ' + $routeParams);
         $scope.contentUrl = 'database/blog/entries/' + $route.current.params.title + '.html';
     }])
 })();
