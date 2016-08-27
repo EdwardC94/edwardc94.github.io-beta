4fc4794a05ba0d68edebc5db8d18f803bf035afa
Eduardo <edwardc94@outlook.com>
2016-08-27 17:56:59 -0500

    Update directives.js

--- angular-scripts/directives.js
+++ angular-scripts/directives.js
@@ -1,1 +1,8 @@
-
+(function () {
+    angular.module("MyWebsiteDirectives", []).
+    directive("imhoBanner", function(){
+        return {
+            templateUrl : 'partials/imho-banner.html'
+        }
+    })
+})();
