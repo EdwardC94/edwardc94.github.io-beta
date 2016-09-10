7dc0ee16d9a7d326e08287784c756e423172dc77
Eduardo <edwardc94@outlook.com>
2016-09-09 21:42:31 -0500

    Update app_controllers.js

--- angular-scripts/app_controllers.js
+++ angular-scripts/app_controllers.js
@@ -7,6 +7,18 @@
         $scope.contentUrl = 'database/blog/entries/' + $routeParams.entry + '.html';
     }]).
     controller('IMHOCtrl', ['$scope', 'data', function($scope, data) {
-        $scope.blogInfo = data;
+        $scope.theData = data;
+        var max = data.length - 2 
+        $scope.dataIndex = 0
+        $scope.move = function (direction) {
+            if (direction == "left") {
+                if($scope.dataIndex > 0)
+                    $scope.dataIndex--;
+            } else if (direction == "right") {
+                if($scope.dataIndex < max)
+                    $scope.dataIndex++;
+            }
+        }
+        $scope.blogInfo = [ $scope.theData[$scope.dataIndex], $scope.theData[$scope.dataIndex+1], $scope.theData[$scope.dataIndex+2] ];
     }])
 })();
