3cd1979486802a5b8a47014bf90a57cab7611272
Eduardo <lol.ed94@hotmail.com>
2016-09-12 09:31:57 -0500

    Ok... :+1:

--- angular-scripts/app_controllers.js
+++ angular-scripts/app_controllers.js
@@ -1,18 +1,29 @@
 (function () {
-    function whereCategory (categoryString, postArray) {
+    function whereCategoryID (categoryID, postArray) {
         var res = [];
         console.log(postArray);
         for (var i = 0; i < postArray.length; i++){
-            if (postArray[i].categoryID_FK == categoryString)
+            if (postArray[i].categoryID_FK == categoryID)
                 res.push(postArray[i]);
         }
         return res.reverse();
+    };
+    function getCategoryIDbyCategoryName (categoryName, categoryArray) {
+        var res;
+        for (var i = 0; i < categoryArray.length; i++){
+            if (categoryArray[i].name == categoryName)
+                res = categoryArray[i].categoryID;
+        }
+        return res;
     };
     angular.module('MyWebsiteCtrls', ['ngRoute']).
     controller('HomeCtrl', ['$scope', function ($scope) {
     }]).
     controller('BlogEntryCtrl', ['$scope', 'data', '$route',  '$routeParams', function($scope, data, $route, $routeParams) {
         $scope.theData = data;
+        var postArray = $scope.theData.Post;
+        var categoryArray = $scope.theData.Category;
+        $scope.catInfo = whereCategoryID(getCategoryIDbyCategoryName($routeParams.category, categoryArray), postArray);
         $scope.contentUrl = 'partials/blog/' + $routeParams.category + '/' + $routeParams.entry + '.html';
     }]).
     controller('IMHOCtrl', ['$scope', 'data', function($scope, data) {
