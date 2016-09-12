f47cff6b69443bec48bfa7e9d0baa2bdacc03002
Eduardo <lol.ed94@hotmail.com>
2016-09-12 08:19:10 -0500

    oooops

--- angular-scripts/app_services.js
+++ angular-scripts/app_services.js
@@ -1,8 +1,8 @@
 (function (){
     var services = angular.module('MyWebsiteServices', ['ngResource']).
-    factory('getEntry', ['$resource', function ($resource) {
-        return $resource('database/blog/entries/:title.json', {}, {
-            getEntry : { method : 'GET', params : { title : '' }, isArray : false }
-      });
-  }])
+    factory('getEntries', ['$resource', function ($resource) {
+        return $resource('database/blog/entries.json', {}, {
+            getEntries : { method : 'GET', params : {}, isArray : false }
+        })
+    }])
 })();
