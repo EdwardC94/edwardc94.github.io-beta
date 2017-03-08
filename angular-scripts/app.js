32cf6220249fe6548afa9c262735d9bbd3c811d1
Eduardo <EdwardC94@users.noreply.github.com>
2017-03-07 22:31:39 -0600

    Update app.js

--- angular-scripts/app.js
+++ angular-scripts/app.js
@@ -15,6 +15,15 @@
 
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
+        when('/home', {
+            templateUrl : 'partials/home.html'
+        }).
+        when('/about', {
+            templateUrl : 'partials/about.html'
+        }).
+        when('/projects', {
+            templateUrl : 'partials/projects.html'
+        })
         when('/blog', {
             templateUrl : 'partials/blog.html',
             controller : 'BlogCTRL',
@@ -23,6 +32,19 @@
                     return getEntries.query().$promise;   
                 }]
             }
+        }).
+        when('/blog/:cat/:post', {
+            templateUrl : 'partials/post.html',
+            controller : "PostCTRL",
+            resolve : {
+                data : ['getEntries', function(getEntries){
+                    return getEntries.query().$promise;   
+                }]
+            }
+        }).
+        when('/404', {
+            templateUrl : 'partials/404.html',
+            controller : "404CTRL"
         }).
         otherwise("/blog")
     }])    
